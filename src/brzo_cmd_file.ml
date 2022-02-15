b0-system/brzo

commit 07482a367bb564c27f3b28164a90f4c64c29549b

Use Cmdliner groups and various cleanups.

src/brzo_cmd_file.ml
@@ -4,6 +4,7 @@
   ---------------------------------------------------------------------------*)
 
 open B00_std
+open Result.Syntax
 open B00_serialk_sexp
 
 (* A few notes that may end up being ux quirks.
@@ -20,15 +21,14 @@ let show_sexp pp sexp =
 
 let get_action_arg parse ~kind v k =
   Log.if_error' ~use:Brzo.Exit.some_error @@
-  match v with
-  | None -> Fmt.error "No %s specified." kind
-  | Some v ->
+  match parse v with
+  | Ok v -> k v
+  | Error _ ->
       (* The parse errors are not really fit/good here we circumvent them
          for now. *)
-      match parse v with
-      | Error _ -> Fmt.error "Could not parse %s argument %S." kind v
-      | Ok v -> k v
+      Fmt.error "Could not parse %s argument %S." kind v
 
+(* FIXME this should be moved to cmdliner cli parsing **)
 let get_spath = get_action_arg Sexp.path_of_string ~kind:"s-expression path"
 let get_caret = get_action_arg Sexp.caret_of_string ~kind:"s-expression caret"
 let get_sexps = get_action_arg Brzo.Sexp.of_string ~kind:"s-expression"
@@ -58,6 +58,7 @@ let sexp_query query sexp k =
   Result.bind (Brzo.Sexp.query query sexp) k
 
 let delete c spath dry_run =
+  Log.if_error ~use:Brzo.Exit.some_error @@
   get_brzo_file c @@ fun file ->
   get_sexp_of_file file @@ fun sexp ->
   get_spath spath @@ fun spath ->
@@ -66,21 +67,24 @@ let delete c spath dry_run =
   Ok Brzo.Exit.ok
 
 let edit c =
+  Log.if_error ~use:Brzo.Exit.some_error @@
   let brzo_file = match Brzo.Conf.brzo_file c with
   | Some file -> file
   | None -> Fpath.(Brzo.Conf.root c / Brzo.Conf.brzo_file_name)
   in
-  Result.bind (B00_editor.find ()) @@ fun editor ->
-  Result.bind (B00_editor.edit_files editor [brzo_file]) @@ function
+  let* editor = B00_editor.find () in
+  let* exit = B00_editor.edit_files editor [brzo_file] in
+  match exit with
   | `Exited 0 -> Ok Brzo.Exit.ok
   | _ -> Ok Brzo.Exit.some_error
 
 let get c spath =
+  Log.if_error ~use:Brzo.Exit.some_error @@
   get_brzo_file c @@ fun file ->
   get_sexp_of_file file @@ fun sexp ->
   match spath with
   | None -> show_sexp Sexp.pp_seq_layout (fst sexp); Ok Brzo.Exit.ok
-  | Some _ ->
+  | Some spath ->
       get_spath spath @@ fun path ->
       sexp_query (Sexpq.path path Sexpq.sexp) sexp @@ fun r ->
       let is_key = function Sexp.Key _ :: _ -> true | _ -> false in
@@ -92,11 +96,13 @@ let get c spath =
       Ok Brzo.Exit.ok
 
 let path c =
+  Log.if_error ~use:Brzo.Exit.some_error @@
   get_brzo_file c @@ fun file ->
   Log.app (fun m -> m "%a" Fpath.pp_unquoted file);
   Ok Brzo.Exit.ok
 
 let set c caret sexps dry_run =
+  Log.if_error ~use:Brzo.Exit.some_error @@
   get_caret caret @@ fun caret ->
   get_sexps sexps @@ fun sexps ->
   get_brzo_file c @@ fun file ->
@@ -106,108 +112,134 @@ let set c caret sexps dry_run =
   update_file ~dry_run file sexp @@ fun () ->
   Ok Brzo.Exit.ok
 
-let file c action spath sexps dry_run =
-  Log.if_error ~use:Brzo.Exit.some_error @@
-  match action with
-  | `Delete -> delete c spath dry_run
-  | `Edit -> edit c
-  | `Get -> get c spath
-  | `Path -> path c
-  | `Set -> set c spath sexps dry_run
-
 (* Command line interface *)
 
 open Cmdliner
 
-let doc = "Query and edit the BRZO file"
-let sdocs = Manpage.s_common_options
-let exits = Brzo.Exit.Info.base_cmd
-let envs = B00_editor.envs ()
-let man_xrefs = [ `Main ]
-let man = [
-  `S Manpage.s_description;
-  `P "The $(tname) command queries and edits the BRZO file.";
-  `P "The file is edited on the command line using s-expression paths \
-      and carets.";
-  `S "PATHS AND CARETS";
-  `P "An s-expression path $(b,SPATH) is a sequence of dot separated \
-      bracketed key dictionary and list indexing operations. Brackets \
-      can be omited if there is no ambiguity. List indices are \
-      zero-based and negative numbers count from the end: -1 is the \
-      last element of a list. Examples:";
-  `Pre "$(b,ocaml.libs)       # value of key $(b,libs) in dictionary \
-        $(b,ocaml)"; `Noblank;
-  `Pre "$(b,ocaml.libs.[0])   # first element of value bound to \
-        $(b,ocaml.libs)"; `Noblank;
-  `Pre "$(b,ocaml.libs.[-1])  # last element of value bound to \
-        $(b,ocaml.libs)";
-  `P "An s-expression caret $(b,CARET) is an s-expression path whose last \
-      index may specify an insertion point $(b,v) before or after the \
-      brackets to denote an insertion before or after the expression found \
-      by the path. Examples:";
-  `Pre "$(b,ocaml.v[libs])     # before key $(b,libs) in dictionary \
-        $(b,ocaml)"; `Noblank;
-  `Pre "$(b,ocaml.libs.v[0])   # before first element of value bound to \
-        $(b,ocaml.libs)"; `Noblank;
-  `Pre "$(b,ocaml.libs.[-1]v)  # after last element of value bound to \
-        $(b,ocaml.libs)";
-  `S "ACTIONS";
-  `I ("$(b,delete) [$(b,--dry-run)] $(i,SPATH)",
-      "Delete the s-expression at path $(i,SPATH) from the BRZO file. If \
-       $(i,SPATH) ends with a key index it removes the binding. If \
-       $(b,--dry-run) is specified the file is not modified and the result \
-       of the operation on the BRZO file is output on $(b,stdout).");
-  `I ("$(b,edit)",
-      "Edit or create the BRZO file in your editor.");
-  `I ("$(b,get) [$(i,SPATH)]",
-      "Get the s-expression bound to path $(i,SPATH). If no path is specified \
-       the whole BRZO file is output. Key bindings are returned as a \
-       sequence of s-expressions.");
-  `I ("$(b,path)",
-      "Show the file path to the BRZO file. Errors if there is none.");
-  `I ("$(b,set) [$(b,--dry-run)] $(i,CARET) $(i,SEXPSEQ)",
-      "Splice the s-expression sequence $(i,SEXPSEQ) (a single cli arg) at the \
-       caret $(i,CARET) in the BRZO file. To set a key binding use \
-       a sequence of s-expressions not a list; the latter sets the first \
-       element of the binding to a list. If $(b,--dry-run) is specified \
-       the file is not modified and the result of the operation on the \
-       BRZO file is output on $(b,stdout).");
-  Brzo.Cli.man_see_manual ]
-
-let action =
-  let action =
-    [ "delete", `Delete; "edit", `Edit; "get", `Get; "path", `Path;
-      "set", `Set; ]
-  in
-  let doc =
-    Fmt.str "The action to perform. $(docv) must be one of %s."
-      (Arg.doc_alts_enum action)
-  in
-  let action = Arg.enum action in
-  Arg.(required & pos 0 (some action) None & info [] ~doc ~docv:"ACTION")
-
-let spath =
-  let doc = "The s-expression path or caret to act on (if applicable)." in
-  Arg.(value & pos 1 (some string) None & info [] ~doc ~docv:"SPATH")
-
-let sexps =
-  let doc =
-    "The $(b,set) value (if applicable). This is a sequence of s-expressions \
-     spliced at the caret. The sequence must be a single cli argument."
-  in
-  Arg.(value & pos 2 (some string) None & info [] ~doc ~docv:"SEXPSEQ")
-
 let dry_run =
-  let doc =
-    "If applicable, do not edit the BRZO file in place but show the result \
-     on stdout."
+  let doc = "Do not edit the BRZO file in place but show the result on stdout."
   in
   Arg.(value & flag & info ["t"; "dry-run"] ~doc)
 
+let subcmd
+    ?(exits = Brzo.Exit.Info.base_cmd) ?(envs = []) name ~doc ~descr term
+  =
+  let man = [`S Manpage.s_description; descr] in
+  Cmd.v (Cmd.info name ~doc ~exits ~envs ~man) term
+
+let path_term = Term.(const path $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file)
+
+(* Commands *)
+
+let delete =
+  let doc = "Delete an s-expression from the BRZO file" in
+  let descr = `Blocks [
+      `P "$(tname) delete the s-expression at path $(i,SPATH) from the BRZO \
+          file. If $(i,SPATH) ends with a key index it removes the binding.";
+      `P "If $(b,--dry-run) is specified the file is not modified and the \
+          result of the operation on the BRZO file is output on $(b,stdout)." ]
+  in
+  let spath =
+    let doc = "The s-expression path to act on. See $(mname) $(b,file --help) \
+               for the syntax." in
+    Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"SPATH")
+  in
+  subcmd "delete" ~doc ~descr
+    Term.(const delete $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $
+          spath $ dry_run)
+
+let edit =
+  let doc = "Edit or create the BRZO file" in
+  let descr = `P "$(tname) edits opens the BRZO file in your editor." in
+  let envs = B00_editor.envs () in
+  subcmd "edit" ~doc ~envs ~descr
+    Term.(const edit $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file)
+
+let get =
+  let doc = "Get an s-expression from the BRZO file" in
+  let descr =
+    `P "$(tname) outputs the s-expression bound to path $(i,SPATH). \
+        If no path is specified the whole BRZO file is output. Key bindings \
+        are returned as a sequence of s-expressions."
+  in
+  let spath =
+    let doc = "The s-expression path to get. See $(mname) $(b,file --help) \
+               for the path syntax." in
+    Arg.(value & pos 1 (some string) None & info [] ~doc ~docv:"SPATH")
+  in
+  subcmd "get" ~doc ~descr
+    Term.(const get $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $ spath)
+
+let path =
+  let doc = "Output the BRZO file path (default command)" in
+  let descr = `P "$(tname) outputs the file path to the BRZO file. \
+                  Errors if there is none."
+  in
+  subcmd "path" ~doc ~descr path_term
+
+let set =
+  let doc = "Set an s-expression in the BRZO file" in
+  let descr = `Blocks [
+      `P "$(tname) splices the s-expression sequence $(i,SEXPSEQ) (a single \
+          cli arg) at the caret $(i,CARET) in the BRZO file.";
+      `P "To set a key binding use a sequence of s-expressions not a list; \
+           the latter sets the first element of the binding to a list.";
+      `P "If $(b,--dry-run) is specified the file is not modified and the \
+          result of the operation on the BRZO file is output on $(b,stdout)."]
+  in
+  let caret =
+    let doc = "The s-expression caret to act on. See $(mname) $(b,file --help) \
+               for the caret syntax."
+    in
+    Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"CARET")
+  in
+  let sexps =
+    let doc =
+      "The $(b,set) value. This is a sequence of s-expressions \
+       spliced at the caret. The sequence must be a single cli argument."
+    in
+    Arg.(required & pos 2 (some string) None & info [] ~doc ~docv:"SEXPSEQ")
+  in
+  subcmd "set" ~doc ~descr
+    Term.(const set $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $
+          caret $ sexps $ dry_run)
+
+let subs = [delete; edit; path; get; set]
 let cmd =
-  Cmd.v (Cmd.info "file" ~doc ~sdocs ~envs ~exits ~man ~man_xrefs)
-    Term.(const file $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $ action $
-          spath $ sexps $ dry_run)
+  let doc = "Query and edit the BRZO file" in
+  let exits = Brzo.Exit.Info.base_cmd in
+  let man = [
+    `S Manpage.s_description;
+    `P "The $(tname) command queries and edits the BRZO file. The default \
+        command is $(b,path).";
+    `P "The file is edited from the command line by using s-expression paths \
+        and carets which are described in a dedicated section below.";
+    `S Manpage.s_commands;
+    `S "PATHS AND CARETS";
+    `P "An s-expression path $(b,SPATH) is a sequence of dot separated \
+        bracketed key dictionary and list indexing operations. Brackets \
+        can be omited if there is no ambiguity. List indices are \
+        zero-based and negative numbers count from the end: -1 is the \
+        last element of a list. Examples:";
+    `Pre "$(b,ocaml.libs)       # value of key $(b,libs) in dictionary \
+          $(b,ocaml)"; `Noblank;
+    `Pre "$(b,ocaml.libs.[0])   # first element of value bound to \
+          $(b,ocaml.libs)"; `Noblank;
+    `Pre "$(b,ocaml.libs.[-1])  # last element of value bound to \
+          $(b,ocaml.libs)";
+    `P "An s-expression caret $(b,CARET) is an s-expression path whose last \
+        index may specify an insertion point $(b,v) before or after the \
+        brackets to denote an insertion before or after the expression found \
+        by the path. Examples:";
+    `Pre "$(b,ocaml.v[libs])     # before key $(b,libs) in dictionary \
+          $(b,ocaml)"; `Noblank;
+    `Pre "$(b,ocaml.libs.v[0])   # before first element of value bound to \
+          $(b,ocaml.libs)"; `Noblank;
+    `Pre "$(b,ocaml.libs.[-1]v)  # after last element of value bound to \
+          $(b,ocaml.libs)";
+    Brzo.Cli.man_see_manual ]
+  in
+  Cmd.group (Cmd.info "file" ~doc ~exits ~man) ~default:path_term subs
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2018 The brzo programmers

src/brzo_cmd_cache.ml
@@ -4,87 +4,127 @@
   ---------------------------------------------------------------------------*)
 
 open B00_std
+open Result.Syntax
 
 let find_used_keys c =
-  Result.bind (Os.Dir.exists (Brzo.Conf.cache_dir c)) @@ function
-  | false -> Ok (String.Set.empty)
-  | true ->
-      Result.map_error (Fmt.str "Cannot determine used keys: %s") @@
-      Result.bind (B00_cli.Memo.Log.read (Brzo.Conf.log_file c)) @@ fun l ->
-      Ok (B00_cli.File_cache.keys_of_done_ops (B00_cli.Memo.Log.ops l))
+  let* exists = Os.Dir.exists (Brzo.Conf.cache_dir c) in
+  if exists then Ok (String.Set.empty) else
+  (Result.map_error (Fmt.str "Cannot determine used keys: %s") @@
+   let* l = B00_cli.Memo.Log.read (Brzo.Conf.log_file c) in
+   Ok (B00_cli.File_cache.keys_of_done_ops (B00_cli.Memo.Log.ops l)))
 
 let get_used_keys c = Result.value ~default:String.Set.empty (find_used_keys c)
 
-let cache c (max_byte_size, pct) (action, keys) =
-  let dir = Brzo.Conf.cache_dir c in
-  let action = match action with
-  | `Delete -> B00_cli.File_cache.delete ~dir keys
-  | `Gc ->
-      Result.bind (find_used_keys c) @@ fun used ->
-      B00_cli.File_cache.gc ~dir ~used
-  | `Keys -> B00_cli.File_cache.keys ~dir
-  | `Path -> Log.app (fun m -> m "%a" Fpath.pp_unquoted dir); Ok true
-  | `Stats -> B00_cli.File_cache.stats ~dir ~used:(get_used_keys c)
-  | `Trim ->
-      let used = get_used_keys c in
-      B00_cli.File_cache.trim ~dir ~used ~max_byte_size ~pct
-  in
+let delete c keys =
   Log.if_error ~use:Brzo.Exit.some_error @@
-  Result.bind action @@ fun _ -> Ok Brzo.Exit.ok
+  let dir = Brzo.Conf.cache_dir c in
+  let* _ = B00_cli.File_cache.delete ~dir keys in
+  Ok Brzo.Exit.ok
+
+let gc c =
+  Log.if_error ~use:Brzo.Exit.some_error @@
+  let dir = Brzo.Conf.cache_dir c in
+  let* used = find_used_keys c in
+  let* _ = B00_cli.File_cache.gc ~dir ~used in
+  Ok Brzo.Exit.ok
+
+let keys c =
+  Log.if_error ~use:Brzo.Exit.some_error @@
+  let dir = Brzo.Conf.cache_dir c in
+  let* _ = B00_cli.File_cache.keys ~dir in
+  Ok Brzo.Exit.ok
+
+let path c =
+  Log.if_error ~use:Brzo.Exit.some_error @@
+  let dir = Brzo.Conf.cache_dir c in
+  Log.app (fun m -> m "%a" Fpath.pp_unquoted dir);
+  Ok Brzo.Exit.ok
+
+let stats c =
+  Log.if_error ~use:Brzo.Exit.some_error @@
+  let dir = Brzo.Conf.cache_dir c in
+  let* _ = B00_cli.File_cache.stats ~dir ~used:(get_used_keys c) in
+  Ok Brzo.Exit.ok
+
+let trim c (max_byte_size, pct) =
+  Log.if_error ~use:Brzo.Exit.some_error @@
+  let dir = Brzo.Conf.cache_dir c in
+  let used = get_used_keys c in
+  let* _ = B00_cli.File_cache.trim ~dir ~used ~max_byte_size ~pct in
+  Ok Brzo.Exit.ok
 
 (* Command line interface *)
 
 open Cmdliner
 
-let doc = "Operate on the build cache"
-let sdocs = Manpage.s_common_options
-let exits = Brzo.Exit.Info.base_cmd
-let man_xrefs = [ `Main; `Tool "b00-cache" ]
-let man = [
-  `S Manpage.s_synopsis;
-  `P "$(mname) $(tname) $(i,ACTION) [$(i,OPTION)]...";
-  `S Manpage.s_description;
-  `P "The $(tname) command operates on the build cache.";
-  `S "ACTIONS";
-  `I ("$(b,delete) [$(i,KEY)]...", "Delete the cache or only the given keys.");
-  `I ("$(b,gc)", "Only keep keys used by the last build.");
-  `I ("$(b,keys)", "List cache keys.");
-  `I ("$(b,path)", "Display the path to the cache (may not exist).");
-  `I ("$(b,stats)", "Show cache statistics.");
-  `I ("$(b,trim) [$(b,--to-pct) $(i,PCT)] [$(b,--to-mb) $(i,MB)]",
-      "Trim the cache to the minimal budget specified. Without options \
-       trims to 50% of the current size. Keys used by the last build \
-       are preserved if possible.");
-  Brzo.Cli.man_see_manual; ]
+let subcmd
+    ?(exits = Brzo.Exit.Info.base_cmd) ?(envs = []) name ~doc ~descr term
+  =
+  let man = [`S Manpage.s_description; descr] in
+  Cmd.v (Cmd.info name ~doc ~exits ~envs ~man) term
 
-let action =
-  let action =
-    [ "delete", `Delete; "gc", `Gc; "keys", `Keys; "path", `Path;
-      "stats", `Stats; "trim", `Trim ]
-  in
-  let doc =
-    Fmt.str "The action to perform. $(docv) must be one of %s."
-      (Arg.doc_alts_enum action)
-  in
-  let action = Arg.enum action in
-  Arg.(required & pos 0 (some action) None & info [] ~doc ~docv:"ACTION")
+let stats_term =
+  Term.(const stats $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file)
 
-let parse_cli =
-  let keys = B00_cli.File_cache.keys_none_is_all ~pos_right:1 () in
-  let parse_cli action keys =
-    let has_keys = match keys with `Keys _ -> true | _ -> false in
-    match has_keys && action <> `Delete with
-    | false -> `Ok (action, keys)
-    | true ->
-        `Error (true,
-                "too many argument, no positional arguments for this action")
-  in
-  Term.(ret (const parse_cli $ action $ keys))
+(* Commands *)
 
+let delete =
+  let doc = "Delete cache or given keys" in
+  let descr = `P "$(tname) deletes cache or given keys." in
+  let keys = B00_cli.File_cache.keys_none_is_all ~pos_right:(-1) () in
+  subcmd "delete" ~doc ~descr
+    Term.(const delete $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $ keys)
+
+let gc =
+  let doc = "Only keep keys used by the last build." in
+  let descr = `P "$(tname) all keys except those used by the last build." in
+  subcmd "gc" ~doc ~descr
+    Term.(const gc $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file)
+
+let keys =
+  let doc = "List cache keys" in
+  let descr = `P "$(tname) lists all cache keys." in
+  subcmd "keys" ~doc ~descr
+    Term.(const keys $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file)
+
+let path =
+  let doc = "Output cache directory path (may not exist)" in
+  let descr = `P "$(tname) outputs the cache directory path." in
+  subcmd "path" ~doc ~descr
+    Term.(const path $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file)
+
+let stats =
+  let doc = "Output cache statistics (default command)" in
+  let descr = `P "$(tname) outputs cache statistics." in
+  subcmd "stats" ~doc ~descr
+    Term.(const stats $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file)
+
+let trim =
+  let doc = "Trim the cache to a given budget." in
+  let descr = `Blocks [
+      `P "$(tname) trims the cache to the minimal given budget. Keys used \
+          by the last build are preserved whenever possible.";
+      `P "Without options trims to 50% of the current size." ]
+  in
+  subcmd "trim" ~doc ~descr
+    Term.(const trim $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $
+          B00_cli.File_cache.trim_cli ())
+
+let subs = [delete; gc; keys; path; stats; trim]
 let cmd =
-  Cmd.v (Cmd.info "cache" ~doc ~sdocs ~exits ~man ~man_xrefs)
-    Term.(const cache $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $
-          B00_cli.File_cache.trim_cli () $ parse_cli)
+  let doc = "Operate on the build cache" in
+  let exits = Brzo.Exit.Info.base_cmd in
+  let man_xrefs = [ `Main; `Tool "b00-cache" ] in
+  let man = [
+    `S Manpage.s_description;
+    `P "The $(tname) command operates on the build cache. The default \
+        command is $(b,stats).";
+    Brzo.Cli.man_see_manual; ]
+  in
+  Cmd.group (Cmd.info "cache" ~doc ~exits ~man ~man_xrefs)
+    ~default:stats_term subs
+
+
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2018 The brzo programmers

src/brzo_cmd_default.ml
@@ -43,34 +43,32 @@ let default c =
       Log.app (fun m -> m "%a" Brzo.Conf.pp_show c); Ok Brzo.Exit.ok
   | r -> Result.bind r @@ fun domain -> Ok (Brzo_domain.run c domain)
 
-
 (* Command line interface *)
 
 open Cmdliner
-
-let doc = "Default domain (default command)" and docs = Brzo.Cli.s_domains
-let sdocs = Manpage.s_common_options
-let exits = Brzo.Exit.Info.domain_cmd
-let man_xrefs = [ `Main ]
-let man = [
-  `S Manpage.s_description;
-  `P "The $(tname) command has the outcomes for the default domain.";
-  `P "The default domain is either automatically selected according to \
-      the sources that are present in the BRZO root or explicitely set \
-      by the BRZO file. Use $(b,brzo domain) to determine which default
-      domain is selected.";
-  `P "See the manual in $(b,odig doc brzo) for more details.";
-  `S Manpage.s_arguments;
-  `S Brzo.Cli.s_outcomes;
-  `S Brzo.Cli.s_outcome_mode;
-  Brzo.Cli.man_see_manual; ]
 
 let term =
   let domain = Brzo_domain.V (module Default) in
   Term.(const default $ Brzo_tie_conf.for_domain ~domain)
 
 let cmd =
-  Cmd.v (Cmd.info "default" ~doc ~docs ~sdocs ~exits ~man ~man_xrefs) term
+  let doc = "Default domain (default command)" in
+  let docs = Brzo.Cli.s_domains in
+  let exits = Brzo.Exit.Info.domain_cmd in
+  let man = [
+    `S Manpage.s_description;
+    `P "The $(tname) command has the outcomes for the default domain.";
+    `P "The default domain is either automatically selected according to \
+        the sources that are present in the BRZO root or explicitely set \
+        by the BRZO file. Use $(b,brzo domain) to determine which default
+      domain is selected.";
+    `P "See the manual in $(b,odig doc brzo) for more details.";
+    `S Manpage.s_arguments;
+    `S Brzo.Cli.s_outcomes;
+    `S Brzo.Cli.s_outcome_mode;
+    Brzo.Cli.man_see_manual; ]
+  in
+  Cmd.v (Cmd.info "default" ~doc ~docs ~exits ~man) term
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2018 The brzo programmers

src/brzo_cmd_delete.ml
@@ -4,6 +4,7 @@
   ---------------------------------------------------------------------------*)
 
 open B00_std
+open Result.Syntax
 
 let delete c clean =
   Log.if_error ~use:Brzo.Exit.some_error @@
@@ -12,32 +13,31 @@ let delete c clean =
   | true -> b0_dir
   | false -> Fpath.(b0_dir / Brzo.Conf.brzo_dir_name)
   in
-  Result.bind (Os.Path.delete ~recurse:true del_dir) @@ fun _ ->
+  let* _ = Os.Path.delete ~recurse:true del_dir in
   Ok Brzo.Exit.ok
 
 (* Command line interface *)
 
 open Cmdliner
 
-let doc = "Delete builds"
-let sdocs = Manpage.s_common_options
-let exits = Brzo.Exit.Info.base_cmd
-let man_xrefs = [ `Main; `Cmd "cache" ]
-let man = [
-  `S Manpage.s_description;
-  `P "The $(tname) command deletes all outcome builds and artefacts. The \
-      build cache is however kept intact.";
-  `P "Use option $(b,--clean) option to get rid of the $(b,_b0) directory \
-      altogeter. The will likely also get rid of the build cache. \
-      See $(mname) $(b,cache) for finer control over build cache deletions.";
-  Brzo.Cli.man_see_manual; ]
-
-let clean =
-  let doc = "Delete the $(b,_b0) directory." in
-  Arg.(value & flag & info ["c"; "clean"] ~doc)
-
 let cmd =
-  Cmd.v (Cmd.info "delete" ~doc ~sdocs ~exits ~man ~man_xrefs)
+  let doc = "Delete builds" in
+  let exits = Brzo.Exit.Info.base_cmd in
+  let man_xrefs = [ `Main; `Cmd "cache" ] in
+  let man = [
+    `S Manpage.s_description;
+    `P "The $(tname) command deletes all outcome builds and artefacts. The \
+        build cache is however kept intact.";
+    `P "Use option $(b,--clean) option to get rid of the $(b,_b0) directory \
+        altogeter. The will likely also get rid of the build cache. \
+        See $(mname) $(b,cache) for finer control over build cache deletions.";
+    Brzo.Cli.man_see_manual; ]
+  in
+  let clean =
+    let doc = "Delete the $(b,_b0) directory." in
+    Arg.(value & flag & info ["c"; "clean"] ~doc)
+  in
+  Cmd.v (Cmd.info "delete" ~doc ~exits ~man ~man_xrefs)
     Term.(const delete $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $ clean)
 
 (*---------------------------------------------------------------------------

src/brzo_cmd_domain.ml
@@ -4,31 +4,31 @@
   ---------------------------------------------------------------------------*)
 
 open B00_std
+open Result.Syntax
 
 let domain c =
   Log.if_error ~use:Brzo.Exit.undefined_domain @@
-  Result.bind (Brzo_domain.of_conf c Brzo_domain_list.v) @@
-  fun d -> Log.app (fun m -> m "@[%s@]" (Brzo_domain.name d)); Ok Brzo.Exit.ok
+  let* d = Brzo_domain.of_conf c Brzo_domain_list.v in
+  Log.app (fun m -> m "@[%s@]" (Brzo_domain.name d));
+  Ok Brzo.Exit.ok
 
 (* Command line interface *)
 
 open Cmdliner
 
-let doc = "Show selected default domain"
-let sdocs = Manpage.s_common_options
-let exits = Brzo.Exit.Info.undefined_domain :: Brzo.Exit.Info.base_cmd
-let man_xrefs = [ `Main ]
-let man = [
-  `S Manpage.s_description;
-  `P "The $(tname) command shows the default domain of a $(mname) \
-      invocation. The default domain is either automatically selected \
-      according to the sources that are present in the BRZO root or \
-      explicitely set by the BRZO file.";
-  `P "See the manual in $(b,odig doc brzo) for more details.";
-  Brzo.Cli.man_see_manual; ]
-
 let cmd =
-  Cmd.v (Cmd.info "domain" ~doc ~sdocs ~exits ~man ~man_xrefs)
+  let doc = "Show selected default domain" in
+  let exits = Brzo.Exit.Info.undefined_domain :: Brzo.Exit.Info.base_cmd in
+  let man = [
+    `S Manpage.s_description;
+    `P "The $(tname) command shows the default domain of a $(mname) \
+        invocation. The default domain is either automatically selected \
+        according to the sources that are present in the BRZO root or \
+        explicitely set by the BRZO file.";
+    `P "See the manual in $(b,odig doc brzo) for more details.";
+    Brzo.Cli.man_see_manual; ]
+  in
+  Cmd.v (Cmd.info "domain" ~doc ~exits ~man)
     Term.(const domain $ Brzo_tie_conf.use_brzo_file)
 
 (*---------------------------------------------------------------------------

commit 7f8fdb077b59d3a187c3b4987258a7b9b47b6adc

b0 file: fix cli positions (regression from 07482a367bb564c27f).

src/brzo_cmd_file.ml
@@ -142,7 +142,7 @@ let delete =
   let spath =
     let doc = "The s-expression path to act on. See $(mname) $(b,file --help) \
                for the syntax." in
-    Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"SPATH")
+    Arg.(required & pos 0 (some string) None & info [] ~doc ~docv:"SPATH")
   in
   subcmd "delete" ~doc ~descr
     Term.(const delete $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $
@@ -165,7 +165,7 @@ let get =
   let spath =
     let doc = "The s-expression path to get. See $(mname) $(b,file --help) \
                for the path syntax." in
-    Arg.(value & pos 1 (some string) None & info [] ~doc ~docv:"SPATH")
+    Arg.(value & pos 0 (some string) None & info [] ~doc ~docv:"SPATH")
   in
   subcmd "get" ~doc ~descr
     Term.(const get $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $ spath)
@@ -191,14 +191,14 @@ let set =
     let doc = "The s-expression caret to act on. See $(mname) $(b,file --help) \
                for the caret syntax."
     in
-    Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"CARET")
+    Arg.(required & pos 0 (some string) None & info [] ~doc ~docv:"CARET")
   in
   let sexps =
     let doc =
       "The $(b,set) value. This is a sequence of s-expressions \
        spliced at the caret. The sequence must be a single cli argument."
     in
-    Arg.(required & pos 2 (some string) None & info [] ~doc ~docv:"SEXPSEQ")
+    Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"SEXPSEQ")
   in
   subcmd "set" ~doc ~descr
     Term.(const set $ Brzo_tie_conf.auto_cwd_root_and_no_brzo_file $